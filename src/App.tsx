0a98646e216d2ae11e48dd0a4ad4cc8fd1ded55e
Dmytro <dmytrostoianov05@gmail.com>
2024-05-23 21:48:22 +0300

    Setup project.
    Initialize project with Vite, React, Typescript, Tailwind, Prettier, ESLint.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <>
+            <h1 className="text-3xl text-purple-600 underline">
+                test tailwind
+            </h1>
+        </>
+    )
+}
+
+export default App
