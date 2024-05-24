a008536cca09aeed4ad8414f3d2c164d140e5224
Dmytro <dmytrostoianov05@gmail.com>
2024-05-24 16:10:28 +0300

    Create basic coin overview page.

--- src/pages/coin-overview.tsx
+++ src/pages/coin-overview.tsx
@@ -3,7 +3,14 @@ import { useParams } from 'react-router-dom'
 const CoinOverview = () => {
     const { coinId } = useParams()
 
-    return <div className="text-white">{coinId}</div>
+    return (
+        <div className="container flex flex-col items-center justify-center gap-8">
+            <header className="flex h-full w-[20rem] flex-col items-center gap-4">
+                <h1 className="text-center text-3xl text-white">{coinId}</h1>
+            </header>
+            <main className="w-full"></main>
+        </div>
+    )
 }
 
 export default CoinOverview
